5f0d2e1b0dd787729d032cd421570398d16e9ea2
조운재 <102952487+chounjae@users.noreply.github.com>
2026-02-27 17:22:29 +0900

    [Silver III] Title: 계단 오르기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,15 +1,19 @@
-num = int(input())
-stairs = list()
-dp = [0] * (num)   # 0~num-1
-for _ in range(num) :
-    stairs.append(int(input())) #0~num-1
-stairs.reverse()
-if num == 1 or num == 2:
-    print(sum(stairs))
-else :
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+stairs = list(int(input()) for _ in range(n))
+dp = [0] * (n)
+
+if n > 1:
     dp[0] = stairs[0]
-    dp[1] = dp[0] + stairs[1]
-    dp[2] = dp[0] + stairs[2]
-    for i in range(3, num) :
-        dp[i] = max(dp[i-3]+stairs[i]+stairs[i-1],dp[i-2]+stairs[i])
-    print(max(dp))
+    dp[1] = stairs[1] + dp[0]
+
+    for i in range(2, n):
+        dp[i] = max(dp[i-2] + stairs[i], dp[i-3] + stairs[i-1] + stairs[i])
+
+else:
+    dp[0] = stairs[0]        
+    
+print(dp[n-1])
